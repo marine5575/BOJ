c4068f11079c40b8ced018831afe970a7ae120d4
Merry Ko <43597548+marine5575@users.noreply.github.com>
2019-07-23 17:49:49 +0900

    Create 3860 할로윈 묘지.cpp

---
+++ 3860 할로윈 묘지.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+using namespace std;
+
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+
+int map[31][31];
+
+
+int main(void) {
+	int w, h, g;
+
+	scanf("%d %d", &w, &h);
+	scanf("%d", &g);
+
+
+
+	return 0;
+}
