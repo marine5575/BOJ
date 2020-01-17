29131c5a86ca5e66787205653d86ac7c1e9cb886
Merry Ko <43597548+marine5575@users.noreply.github.com>
2020-01-17 20:10:13 +0900

    Update **dkd.cpp

--- **dkd.cpp
+++ **dkd.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
-#include <cmath>
+#include <cmath>	// abs
 #include <queue>
 
 using namespace std;
 
 typedef struct ball {
-	int rx, ry, bx, by;
+	int rx, ry, bx, by;	// 빨간 공의 (x, y), 파란 공의 (x, y) 
 }ball;
 
-int map[11][11], visited[11][11][11][11];
-int dx[4] = {0, 0, -1, 1}, dy[4] = {-1, 1, 0, 0};
-int irx, iry, ibx, iby, hx, hy;
-int cnt = 0, ans = 0;
+int map[11][11];	// map[j][i] = -1 or 0 or 1 : (i, j) 위치는 벽 or 길 or 구멍
+int visited[11][11][11][11];	//	visited[rx][ry][bx][by] : 방문 여부 
+int dx[4] = {0, 0, -1, 1}, dy[4] = {-1, 1, 0, 0};	// up, down, left, right 
+int irx, iry, ibx, iby, hx, hy;	// 처음 (rx, ry), (bx, by), 구멍의 위치 (hx, hy) 
+int cnt = 0, ans = 0;	// bfs 깊이, 정답 
 
-queue<ball> q;
+queue<ball> q;	// bfs의 핵심 
 
 void bfs(void) {
+	// queue가 빌 때까지 
 	while(!q.empty()) {
-		int qLen = q.size();
+		int qLen = q.size();	// 원래 가지고 있던 이번 depth 소속 개수 
 		
+		// 갯수만큼 반복 
 		while(qLen--) {
+			// 현재 공들의 위치 & queue에서 빼기 
 			ball cur = q.front(); q.pop();
 			
+			// 구멍에 빠졌을 때 
+			if(cur.rx == hx && cur.ry == hy) {
+				ans = cnt;
+				return;
+			}
+			
+			// 동서남북 다 가보기 
 			for(int i = 0; i < 4; i++) {
 				int rx = cur.rx, ry = cur.ry;
 				int bx = cur.bx, by = cur.by;
 				
+				// 빨간 공 굴리기 
 				while(1) {
 					rx += dx[i]; ry += dy[i];
 					
@@ -70,12 +82,22 @@ void bfs(void) {
 				}
 				
 				if(!visited[rx][ry][bx][by]) {
-					q.push({rx, ry, bx, by});
-					
+					ball next = {rx, ry, bx, by};
+					q.push(next);
+					visited[rx][ry][bx][by] = 1;
 				}
 			}
 		}
+		
+		if(cnt == 10) {
+			ans = -1;
+			return;
+		}
+		cnt++;
 	}
+	
+	ans = -1;
+	return;
 }
 
 
@@ -117,9 +139,12 @@ int main(void) {
 		}
 	}
 	
-	q.push({irx, iry, ibx, iby});
+	ball init = {irx, iry, ibx, iby};
+	q.push(init);
+	visited[irx][iry][ibx][iby] = 1;
+	bfs();
 	
-	
+	cout << ans << "\n";
 	
 	return 0;
 }
