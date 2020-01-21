b4bc955be70d3f534550830065fbf10ecc17e356
Merry Ko <43597548+marine5575@users.noreply.github.com>
2020-01-21 16:46:50 +0900

    Create **dsdfs.cpp

---
+++ **dsdfs.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, maxi = -1;	// map의 크기, 최대값 
+int map[21][21];	// map
+
+// 이동하는 함수 
+void shift(int dir) {
+	queue<int> q;
+	
+	switch(dir) {
+		// up
+		case 0: {
+			// x좌표 
+			for(int i = 0; i < n; i++) {
+				// y좌표 
+				for(int j = 0; j < n; j++) {
+					// 값이 들어있는가? 
+					if(map[j][i]) q.push(map[j][i]);
+					map[j][i] = 0;
+				}
+				
+				int idx = 0;	// 위로 올라가므로 가장 위에서부터 시작 
+				
+				while(!q.empty()) {
+					// 현재 값 
+					int cur = q.front(); q.pop();
+					
+					// 비어있다면 값 놓기 
+					if(!map[idx][i]) map[idx][i] = cur;
+					// 나랑 같은 값인가? 
+					else if(map[idx][i] == cur) {
+						map[idx][i] *= 2;	// 합치기 
+						idx++;	// 다시 함쳐지지 않으므로 
+					}
+					// 아예 다른 값 
+					else {
+						idx++;
+						map[idx][i] = cur;
+					}
+				}
+			}
+			
+			break;
+		}
+		case 1: {
+			for(int i = 0; i < n; i++) {
+				for(int j = n - 1; j >= 0; j--) {
+					if(map[j][i]) q.push(map[j][i]);
+					map[j][i] = 0;
+				}
+				
+				int idx = n - 1;
+			
+				while(!q.empty()) {
+					int cur = q.front(); q.pop();
+					
+					if(!map[idx][i]) map[idx][i] = cur;
+					else if(map[idx][i] == cur) {
+						map[idx][i] *= 2;
+						idx--;
+					}
+					else {
+						idx--;
+						map[idx][i] = cur;
+					}
+				}
+			}
+			
+			break;
+		}
+		case 2: {
+			for(int j = 0; j < n; j++) {
+				for(int i = 0; i < n; i++) {
+					if(map[j][i]) q.push(map[j][i]);
+					map[j][i] = 0;
+				}
+				
+				int idx = 0;
+				
+				while(!q.empty()) {
+					int cur = q.front(); q.pop();
+					
+					if(!map[j][idx]) map[j][idx] = cur;
+					else if(map[j][idx] == cur) {
+						map[j][idx] *= 2;
+						idx++;
+					}
+					else {
+						idx++;
+						map[j][idx] = cur;
+					}
+				}
+			}
+			
+			break;
+		}
+		case 3: {
+			for(int j = 0; j < n; j++) {
+				for(int i = n - 1; i >= 0; i--) {
+					if(map[j][i]) q.push(map[j][i]);
+					map[j][i] = 0;
+				}
+				
+				int idx = n - 1;
+				
+				while(!q.empty()) {
+					int cur = q.front(); q.pop();
+					
+					if(!map[j][idx]) map[j][idx] = cur;
+					else if(map[j][idx] == cur) {
+						map[j][idx] *= 2;
+						idx--;
+					}
+					else {
+						idx--;
+						map[j][idx] = cur;
+					}
+				}
+			}
+			
+			break;
+		}
+	}
+}
+
+void dfs(int cnt) {
+	if(cnt == 5) {
+		for(int j = 0; j < n; j++) {
+			for(int i = 0; i < n; i++) {
+				if(maxi < map[j][i]) maxi = map[j][i];
+			}
+		}
+		
+		return;
+	}
+	
+	int copy[21][21] = {0};
+	
+	for(int j = 0; j < n; j++) {
+		for(int i = 0; i < n; i++) {
+			copy[j][i] = map[j][i];
+		}
+	}
+	
+	for(int k = 0; k < 4; k++) {
+		shift(k);
+		dfs(cnt + 1);
+		
+		for(int j = 0; j < n; j++) {
+			for(int i = 0; i < n; i++) {
+				map[j][i] = copy[j][i];
+			}
+		}
+	}
+}
+
+
+int main(void) {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	
+	cin >> n;
+	
+	for(int j = 0; j < n; j++) {
+		for(int i = 0; i < n; i++) {
+			cin >> map[j][i];
+		}
+	}
+	
+	dfs(0);
+	
+	cout << maxi << "\n";
+	
+	return 0;
+} 
