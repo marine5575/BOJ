746612f25d1151621b292df542a8cb4990af5e4a
Merry Ko <43597548+marine5575@users.noreply.github.com>
2020-01-22 10:51:58 +0900

    Update **dsdfs.cpp

--- **dsdfs.cpp
+++ **dsdfs.cpp
@@ -45,23 +45,31 @@ void shift(int dir) {
 			
 			break;
 		}
+		// down
 		case 1: {
+			// x좌표 
 			for(int i = 0; i < n; i++) {
+				// y좌표 
 				for(int j = n - 1; j >= 0; j--) {
+					// 밑부터 값을 가지고 있는 것을 집어넣음 
 					if(map[j][i]) q.push(map[j][i]);
-					map[j][i] = 0;
+					map[j][i] = 0; 
 				}
 				
-				int idx = n - 1;
+				int idx = n - 1;	// 놓여야 하는 위치 
 			
 				while(!q.empty()) {
-					int cur = q.front(); q.pop();
+					// 내려올 값 
+					int cur = q.front(); q.pop(); 
 					
+					// 비어있으면 걍 놓음 
 					if(!map[idx][i]) map[idx][i] = cur;
+					// 같은 값인가? 
 					else if(map[idx][i] == cur) {
 						map[idx][i] *= 2;
-						idx--;
+						idx--;	// 다시 합쳐지지 않음 
 					}
+					// 다른 값인가? 
 					else {
 						idx--;
 						map[idx][i] = cur;
@@ -71,23 +79,29 @@ void shift(int dir) {
 			
 			break;
 		}
+		// left
 		case 2: {
+			// y좌표 
 			for(int j = 0; j < n; j++) {
+				// x좌표 
 				for(int i = 0; i < n; i++) {
 					if(map[j][i]) q.push(map[j][i]);
 					map[j][i] = 0;
 				}
 				
-				int idx = 0;
+				int idx = 0;	// 놓여져야 하는 위치 
 				
 				while(!q.empty()) {
 					int cur = q.front(); q.pop();
 					
+					// 비어있으면 걍 놓기 
 					if(!map[j][idx]) map[j][idx] = cur;
+					// 같은 값인가? 
 					else if(map[j][idx] == cur) {
 						map[j][idx] *= 2;
-						idx++;
+						idx++;	// 다시 합쳐지진 않음 
 					}
+					// 다른 값인가? 
 					else {
 						idx++;
 						map[j][idx] = cur;
@@ -97,23 +111,29 @@ void shift(int dir) {
 			
 			break;
 		}
+		// right
 		case 3: {
+			// y좌표 
 			for(int j = 0; j < n; j++) {
+				// x좌표 
 				for(int i = n - 1; i >= 0; i--) {
 					if(map[j][i]) q.push(map[j][i]);
 					map[j][i] = 0;
 				}
 				
-				int idx = n - 1;
+				int idx = n - 1;	// 놓여져야 하는 위치 
 				
 				while(!q.empty()) {
 					int cur = q.front(); q.pop();
 					
+					// 비어있으면 걍 놓기 
 					if(!map[j][idx]) map[j][idx] = cur;
+					// 같은 값인가? 
 					else if(map[j][idx] == cur) {
 						map[j][idx] *= 2;
-						idx--;
+						idx--;	// 다시 합쳐지지 않음 
 					}
+					// 다른 값인가? 
 					else {
 						idx--;
 						map[j][idx] = cur;
@@ -127,6 +147,7 @@ void shift(int dir) {
 }
 
 void dfs(int cnt) {
+	// 5번 움직임 => 최댓값 확인 
 	if(cnt == 5) {
 		for(int j = 0; j < n; j++) {
 			for(int i = 0; i < n; i++) {
@@ -137,7 +158,7 @@ void dfs(int cnt) {
 		return;
 	}
 	
-	int copy[21][21] = {0};
+	int copy[21][21] = {0};	// 배열 backup 
 	
 	for(int j = 0; j < n; j++) {
 		for(int i = 0; i < n; i++) {
@@ -145,10 +166,12 @@ void dfs(int cnt) {
 		}
 	}
 	
+	// up, down, left, right
 	for(int k = 0; k < 4; k++) {
-		shift(k);
-		dfs(cnt + 1);
+		shift(k);	// 움직이고 
+		dfs(cnt + 1);	// 다음 움직임으로 
 		
+		// 방문 해제 
 		for(int j = 0; j < n; j++) {
 			for(int i = 0; i < n; i++) {
 				map[j][i] = copy[j][i];
