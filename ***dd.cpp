834748075d6e3533cde0438c5a19f9310d317a76
Merry Ko <43597548+marine5575@users.noreply.github.com>
2020-01-23 11:09:27 +0900

    Create ***dd.cpp

---
+++ ***dd.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int map[501][501];
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+int maxi = -1;
+
+int dfs(int x, int y, int cnt) {
+	if(cnt == 4) {
+		return map[y][x];
+	}
+	
+	int sum = 0;
+	
+	for(int i = 0; i < 4; i++) {
+		int nx = x + dx[i], ny = y + dy[i];
+		
+		
+	}
+}
+
+
+int main(void) {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	
+	int n, m;
+	cin >> n >> m;
+	
+	for(int j = 0; j < n; j++) {
+		for(int i = 0; i < m; i++) {
+			cin >> map[j][i];
+		}
+	}
+	
+	
+	
+	return 0;
+}
