072835e4c07e4860a92c6672022ab9149207f0d7
Merry Ko <43597548+marine5575@users.noreply.github.com>
2020-01-23 13:06:01 +0900

    Update ***dd.cpp

--- ***dd.cpp
+++ ***dd.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int map[501][501];
+int map[501][501], visited[501][501];
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {-1, 1, 0, 0};
 int maxi = -1;
@@ -17,7 +17,15 @@ int dfs(int x, int y, int cnt) {
 	for(int i = 0; i < 4; i++) {
 		int nx = x + dx[i], ny = y + dy[i];
 		
+		if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
 		
+		if(!visited[ny][nx]) {
+			visited[ny][nx] = 1;
+			sum += dfs(nx, ny, cnt + 1);
+			
+			if(maxi < sum) maxi = sum;
+			visited[ny][nx] = 0;
+		}
 	}
 }
 
